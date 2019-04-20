035701ac08e4e5e24c6da756ea2f7df73b69b68a
Ygor <ygor.maniz@gmail.com>
2019-04-20 02:42:39 -0300

    Implementing first activity

---
+++ app/src/main/java/com/example/finances/ui/activity/ListTransactionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finances.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.finances.R
+
+class ListTransactionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_transactions)
+    }
+}
